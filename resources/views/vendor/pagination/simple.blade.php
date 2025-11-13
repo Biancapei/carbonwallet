9e8f99960dc66c39406becf263857dd20b9dfe06
Bianca Pei <biancapei@Carbons-MacBook-Air.local>
2025-11-13 11:28:25 +0800

    Add results count text below pagination links

--- resources/views/vendor/pagination/simple.blade.php
+++ resources/views/vendor/pagination/simple.blade.php
@@ -1,24 +1,38 @@
 @if ($paginator->hasPages())
-    <nav role="navigation" aria-label="Pagination Navigation" class="blog-pagination-nav" data-pagination>
-        <div class="blog-pagination">
-            {{-- Pagination Elements --}}
-            @foreach ($elements as $element)
-                {{-- "Three Dots" Separator --}}
-                @if (is_string($element))
-                    <span class="blog-pagination-ellipsis">{{ $element }}</span>
-                @endif
+    <div style="display: flex; flex-direction: column; align-items: center; gap: 1rem;">
+        <nav role="navigation" aria-label="Pagination Navigation" class="blog-pagination-nav" data-pagination>
+            <div class="blog-pagination">
+                {{-- Pagination Elements --}}
+                @foreach ($elements as $element)
+                    {{-- "Three Dots" Separator --}}
+                    @if (is_string($element))
+                        <span class="blog-pagination-ellipsis">{{ $element }}</span>
+                    @endif
 
-                {{-- Array Of Links --}}
-                @if (is_array($element))
-                    @foreach ($element as $page => $url)
-                        @if ($page == $paginator->currentPage())
-                            <span class="blog-pagination-link active" aria-current="page">{{ $page }}</span>
-                        @else
-                            <a class="blog-pagination-link" href="{{ $url }}" data-page="{{ $page }}" data-url="{{ $url }}">{{ $page }}</a>
-                        @endif
-                    @endforeach
-                @endif
-            @endforeach
-        </div>
-    </nav>
+                    {{-- Array Of Links --}}
+                    @if (is_array($element))
+                        @foreach ($element as $page => $url)
+                            @if ($page == $paginator->currentPage())
+                                <span class="blog-pagination-link active" aria-current="page">{{ $page }}</span>
+                            @else
+                                <a class="blog-pagination-link" href="{{ $url }}" data-page="{{ $page }}" data-url="{{ $url }}">{{ $page }}</a>
+                            @endif
+                        @endforeach
+                    @endif
+                @endforeach
+            </div>
+        </nav>
+        
+        @if ($paginator->total() > 0)
+            <p class="text-sm text-gray-700 leading-5 dark:text-gray-400" style="margin: 0; text-align: center;">
+                Showing
+                <span class="font-medium">{{ $paginator->firstItem() }}</span>
+                to
+                <span class="font-medium">{{ $paginator->lastItem() }}</span>
+                of
+                <span class="font-medium">{{ $paginator->total() }}</span>
+                results
+            </p>
+        @endif
+    </div>
 @endif
